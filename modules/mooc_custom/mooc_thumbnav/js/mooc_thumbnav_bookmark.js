e8098c7618a88e6be9b84ad2b9c43922ad1ee808
btopro <bto108@psu.edu>
2013-08-19 13:31:25 -0400

    cleaned up UX / UI and performance of thumbnav

--- modules/mooc_custom/mooc_thumbnav/js/mooc_thumbnav_bookmark.js
+++
@@ -1,9 +0,0 @@
-(function ($) {
-  $(document).ready(function(){
-    // if next is clicked go to the next page
-    $('.thumbnav_controller img.mooc_thumbnav_bookmark').click(function(){
-      alert(Drupal.t('Bookmark this page'));
-      // TODO: Handle bookmark request
-    });
-  });
-})(jQuery);
--- modules/mooc_custom/mooc_thumbnav/js/mooc_thumbnav_helpdesk.js
+++
@@ -1,9 +0,0 @@
-(function ($) {
-  $(document).ready(function(){
-    // if next is clicked go to the next page
-    $('.thumbnav_controller img.mooc_thumbnav_helpdesk').click(function(){
-      // trigger feedback reloaded click event
-      $('#feedback_button').click();
-    });
-  });
-})(jQuery);
