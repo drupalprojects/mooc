fc3381dcea7dd50c9b66f094a7f6ce696e7fa339
btopro <bto108@psu.edu>
2013-08-19 15:36:36 -0400

    improved mobile accessibility and UX on landscape

--- modules/mooc_custom/mooc_thumbnav/js/mooc_thumbnav_outline.js
+++ modules/mooc_custom/mooc_thumbnav/js/mooc_thumbnav_outline.js
@@ -1,19 +1,29 @@
 (function ($) {
   $(document).ready(function(){
     $('.thumbnav_controller img.mooc_thumbnav_outline').click(function(){
-      var text = '';
-      var title = $(this).attr('alt');
-      if ($('#block-system-main-menu').length > 0) {
-        text += $('#block-system-main-menu').html();
+      if (!$(this).hasClass('mooc_thumbnav_outline-active')) {
+        $(this).addClass('mooc_thumbnav_outline-active');
+        $(this).attr('src', $(this).attr('src').replace('-click.png', '.png'));
+        var text = '';
+        var title = $(this).attr('alt');
+        if ($('#block-system-main-menu').length > 0) {
+          text += $('#block-system-main-menu').html();
+        }
+        if ($('#block-cis-service-connection-active-outline').length > 0) {
+          text += $('#block-cis-service-connection-active-outline').html();
+        }
+        Drupal.thumbnav.modal(title, text);
+        // add click event for tinynav support it exists
+        $('#thumbnav_modal select').change(function () {
+          window.location.href = $(this).val();
+        });
       }
-      if ($('#block-cis-service-connection-active-outline').length > 0) {
-        text += $('#block-cis-service-connection-active-outline').html();
+      else {
+        // drop class and trigger a click to act like a toggle
+        $(this).removeClass('mooc_thumbnav_outline-active');
+        $(this).attr('src', $(this).attr('src').replace('.png', '-click.png'));
+        $('#thumbnav_modal_close').click();
       }
-      Drupal.thumbnav.modal(title, text);
-      // add click event for tinynav support it exists
-      $('#thumbnav_modal select').change(function () {
-        window.location.href = $(this).val();
-      });
     });
   });
 })(jQuery);
