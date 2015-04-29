4719a5aba8c440f39e24379ddd64a266a1f79ece
btopro <bto108@psu.edu>
2015-04-29 08:38:19 -0400

    abstracted from foundation_access

---
+++ themes/mooc_foundation_access/template.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * Implements template_preprocess_page.
+ */
+function mooc_foundation_access_preprocess_page(&$variables) {
+  // speedreader is enabled
+  if (module_exists('speedreader')) {
+    $variables['speedreader'] = TRUE;
+  }
+  // mespeak is enabled
+  if (module_exists('mespeak')) {
+    $variables['mespeak'] = TRUE;
+  }
+  if (user_access('access printer-friendly version')) {
+  $variables['tabs_extras'] = '<hr>
+    <li>' . l(t('Print'), 'book/export/html/' . arg(1)) . '</li>';
+  }
+  if (user_access('access contextual links')) {
+    $variables['tabs_extras'] .= '<hr>
+    <li class="cis_accessibility_check"></li>
+    <hr>
+    <li><a href="#" data-reveal-id="block-menu-menu-course-tools-menu-nav-modal">' . t('Course Settings') . '</a></li>';
+  }
+}
