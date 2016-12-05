e0af122dc41ab642c158d4b24382f3f45e21b1f1
btopro <btopro@outlook.com>
2016-12-05 11:32:23 -0500

    table of contents cleaned up

--- themes/mooc_foundation_access/templates/blocks/block--mooc-helper--mooc-helper-toc.tpl.php
+++ themes/mooc_foundation_access/templates/blocks/block--mooc-helper--mooc-helper-toc.tpl.php
@@ -1,5 +1,10 @@
-<div id="<?php print $block_html_id ?>-nav-modal" class="reveal-modal etb-nav-modal disable-scroll" data-reveal tabindex="-1">
-<?php print l('<div class="mooc-helper-toc-icon icon-courses-black etb-modal-icons"></div><span>Home</span>', '<front>', array('html' => TRUE, 'attributes' => array('class' => array('mooc-home-button'))));?><h3><?php print t('Table of Contents'); ?></h3>
-<?php print $content; ?>
-<a class="close-reveal-modal" aria-label="Close">&#215;</a>
-</div>
+<section id="<?php print $block_html_id ?>-nav-modal" class="modal elmsln-modal elmsln-modal-hidden disable-scroll" tabindex="-1" aria-label="<?php print t('Table of contents'); ?>" aria-hidden="true" role="dialog">
+  <div class="center-align valign-wrapper elmsln-modal-title-wrapper cis-lmsless-background cis-lmsless-border">
+  <?php print l('<i class="material-icons black-text left">home</i><span class="hide-on-med-and-down">' . t('Home') . '</span>', '<front>', array('html' => TRUE, 'attributes' => array('data-voicecommand' => t('home'), 'class' => array('waves-effect waves-light btn-large white black-text mooc-home-button'))));?>
+  <h2 class="flow-text valign elmsln-modal-title"><?php print t('Table of contents'); ?></h2>
+  <a href="#" class="close-reveal-modal" aria-label="<?php print t('Close'); ?>" data-voicecommand="close (menu)" data-jwerty-key="Esc">&#215;</a>
+  </div>
+  <div class="mooc-outline-modal-content elmsln-scroll-bar">
+    <?php print $content; ?>
+  </div>
+</section>
